b57643299ba9b1783114b21b69cc29aa1c52c57b
Daniel Mitsuo | DanMit <danielmokamoto@gmail.com>
2025-08-21 20:48:26 -0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/renderer.ts',
+  target: 'electron-renderer',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: { extensions: ['.ts', '.js'] },
+  output: {
+    filename: 'renderer.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
