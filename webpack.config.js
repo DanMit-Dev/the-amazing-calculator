35b295b54b811bca2f359cc198579de87d929e34
Daniel Mitsuo | DanMit <danielmokamoto@gmail.com>
2025-08-22 15:06:26 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,34 @@
+
 const path = require('path');
 
-module.exports = {
-  entry: './src/renderer.ts',
-  target: 'electron-renderer',
-  module: {
-    rules: [
-      { test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }
-    ]
+module.exports = [
+  // renderer bundle (bundles mathjs + renderer into dist/renderer.js)
+  {
+    entry: './src/renderer.ts',
+    target: 'electron-renderer',
+    module: {
+      rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }]
+    },
+    resolve: { extensions: ['.ts', '.js'] },
+    output: {
+      filename: 'renderer.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'production'
   },
-  resolve: { extensions: ['.ts', '.js'] },
-  output: {
-    filename: 'renderer.js',
-    path: path.resolve(__dirname, 'dist')
+  // main process compile (TS -> dist/main.js)
+  {
+    entry: './src/main.ts',
+    target: 'electron-main',
+    module: {
+      rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }]
+    },
+    resolve: { extensions: ['.ts', '.js'] },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    node: { __dirname: false, __filename: false },
+    mode: 'production'
   }
-};
+];
